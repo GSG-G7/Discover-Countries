efa0775def9e837eb9c6b6e0829235ce4b8faf66
asem1789 <as.1789@hotmail.com>
2019-07-24 11:19:01 +0300

    write code in dom file, for make request and filter of data Realtes #17

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,12 @@
 const apiRequest = (url, callback) => {
   const xhr = new XMLHttpRequest();
-  console.log(xhr);
+  // console.log(xhr);
   xhr.onreadystatechange = (() => {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = (JSON.stringify(xhr.responseText));
+      const data = (JSON.parse(xhr.responseText));
       callback(data);
     }
   })
   xhr.open('GET', url, true);
   xhr.send();
 };
-
-apiRequest('/API', (res) => {
-  console.log(res);
-});
